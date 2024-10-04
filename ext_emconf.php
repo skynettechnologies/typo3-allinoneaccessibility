87de9932987f713cc78febb01ddf5efd8fc8d34b
Skynetdeveloper <developer3@skynettechnologies.com>
2024-10-04 12:44:12 +0530

    Update All in One Accessibility™ to All in One Accessibility®

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 
 $EM_CONF['allinoneaccessibility'] = [
-    'title' => 'All in One Accessibility',
+    'title' => 'All in One Accessibility®',
     'description' => 'WCAG Website Accessibility widget improves Typo3 website ADA compliance and browser experience for ADA, WCAG 2.1 & 2.2, Section 508, Australian DDA, European EAA EN 301 549, UK Equality Act (EA), Israeli Standard 5568, California Unruh, Ontario AODA, Canada ACA, German BITV, France RGAA, Brazilian Inclusion Law (LBI 13.146/2015), Spain UNE 139803:2012, JIS X 8341 (Japan), Italian Stanca Act and Switzerland DDA Standards.',
     'category' => 'plugin',
     'author' => 'Skynet Technologies USA LLC',
@@ -10,7 +10,7 @@ $EM_CONF['allinoneaccessibility'] = [
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
-    'version' => '2.2.4',
+    'version' => '2.2.5',
     'constraints' => [
         'depends' => [
             'typo3' => '12.0.0-12.4.99',
